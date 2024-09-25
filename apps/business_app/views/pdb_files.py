014334e87acb360e0f0a5b14cc774dd3363dc9a6
Drak <ealmeida.cu@gmail.com>
2024-09-25 11:55:06 -0400

    fix

--- apps/business_app/views/pdb_files.py
+++ apps/business_app/views/pdb_files.py
@@ -1,19 +1,22 @@
-from rest_framework import permissions
+from rest_framework import permissions, viewsets
 from rest_framework.generics import GenericAPIView
 
 
 from apps.business_app.models import PdbFiles
 from rest_framework import mixins
 
+from apps.business_app.serializers.pdb_files import PdbFilesSerializer
+
 
 # Create your views here.
 
 
-class PdbFileViewSet(mixins.RetrieveModelMixin, GenericAPIView):
+class PdbFileViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     API endpoint that allows to compute changes on graph data.
     """
 
-    queryset = PdbFiles.objects.filter(kind=PdbFiles.KIND.GRAPH_GENERATED)
+    queryset = PdbFiles.objects.filter(kind=PdbFiles.KIND.GRAPH_GENERATED).all()
+    serializer_class = PdbFilesSerializer
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
