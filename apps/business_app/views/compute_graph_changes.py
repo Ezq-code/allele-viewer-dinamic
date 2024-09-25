754bfa171e21d0dde66f14e90b237af5656d80f0
julioquintanazaez <jupyterdevs2022@gmail.com>
2024-09-25 15:54:55 +0100

    algo

--- apps/business_app/views/compute_graph_changes.py
+++ apps/business_app/views/compute_graph_changes.py
@@ -4,26 +4,19 @@ from rest_framework.generics import GenericAPIView
 
 
 from apps.common.views import CommonOrderingFilter
-from apps.business_app.models import UploadedFiles
+from apps.business_app.models import PdbFiles
 from apps.business_app.serializers import UploadedFilesSerializer
 
 
 # Create your views here.
 
 
-class ComputeGraphChangesViewSet(viewsets.ModelViewSet, GenericAPIView):
+class PdbFileViewSet(viewsets.ModelViewSet, GenericAPIView):
     """
     API endpoint that allows to compute changes from graph files only.
     """
 
-    queryset = UploadedFiles.objects.all().prefetch_related("GRAPH")
-    serializer_class = UploadedFilesSerializer
-    search_fields = ["custom_name", "description"]
-    ordering_fields = "__all__"
-    filter_backends = [
-        DjangoFilterBackend,
-        filters.SearchFilter,
-        CommonOrderingFilter,
-    ]
+    queryset = PdbFiles.objects.filter("GRAPH")
+    
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
