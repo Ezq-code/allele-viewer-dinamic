2412d67f55074975e1a6604415997636f4210db9
Enrique A. <ealmeida.cu@gmail.com>
2024-11-26 00:02:20 -0500

    added viewset for Event

---
+++ apps/business_app/views/event.py
@@ -0,0 +1,27 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, permissions, viewsets
+from rest_framework.generics import GenericAPIView
+
+from apps.business_app.serializers.event import EventSerializer
+from apps.common.views import CommonOrderingFilter
+from apps.business_app.models import Event
+from apps.business_app.serializers import AllowedExtensionsSerializer
+
+# Create your views here.
+
+
+class EventViewSet(viewsets.ModelViewSet, GenericAPIView):
+
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    search_fields = [
+        "event_name",
+    ]
+    ordering_fields = "__all__"
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        CommonOrderingFilter,
+    ]
+
+    permission_classes = [permissions.AllowAny]
