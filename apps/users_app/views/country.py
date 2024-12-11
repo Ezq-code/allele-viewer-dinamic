f30db034de21b635a5730cb53621abf28927d7d9
Drak <ealmeida.cu@gmail.com>
2024-12-11 09:52:41 -0500

    get codes code complete

--- apps/users_app/views/country.py
+++ apps/users_app/views/country.py
@@ -1,10 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+
 
 from apps.common.views import GetAllMixin
 from apps.users_app.models import Country
 from apps.users_app.serializers import CountrySerializer
+from rest_framework.decorators import action
+
 
 # Create your views here.
 
@@ -22,6 +26,7 @@ class CountryViewSet(
     serializer_class = CountrySerializer
     search_fields = [
         "name",
+        "region_country__region__name"
     ]
     filterset_fields = [
         "enabled",
@@ -33,3 +38,13 @@ class CountryViewSet(
     ordering_fields = "__all__"
 
     permission_classes = [permissions.AllowAny]
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_name="get-codes",
+        url_path="get-codes",
+    )
+    def get_codes(self, request):
+        codes = self.filter_queryset(self.get_queryset()).values_list("code", flat=True)
+        return Response(codes)
