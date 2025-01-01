822019e9a4d9fe9445296e0dabf22ae26f288b68
Duniesky <dunieskydp86@gmail.com>
2025-01-01 11:21:01 -0500

    fix mapa js

--- static/assets/dist/js/mapa.js
+++ static/assets/dist/js/mapa.js
@@ -29,164 +29,62 @@ jQuery("#world-map").vectorMap({
     ],
   },
 
-//   onRegionClick: function (element, code, region) {
-//     var message =
-//       'You clicked "' + region + '" which has the code: ' + code.toUpperCase();
-
-//     alert(message);
-//   },
+  onRegionClick: function (element, code, region) {
+    var message =
+      'You clicked "' + region + '" which has the code: ' + code.toUpperCase();
+    alert(message);
+  },
 });
-
 jQuery("#world-map").on("drag", function (event) {});
 
-const countriesByRegion = {
-  Africa: [
-    "ao",
-    "dz",
-    "bj",
-    "bw",
-    "bf",
-    "bi",
-    "cm",
-    "cv",
-    "cf",
-    "cg",
-    "dj",
-    "eg",
-    "gq",
-    "er",
-    "ma",
-    "et",
-    "ga",
-    "gh",
-    "gm",
-    "gn",
-    "gw",
-    "ke",
-    "ls",
-    "lr",
-    "mg",
-    "ml",
-    "mr",
-    "mu",
-    "mw",
-    "na",
-    "ne",
-    "ng",
-    "rw",
-    "sc",
-    "sd",
-    "sl",
-    "sn",
-    "so",
-    "sz",
-    "td",
-    "tg",
-    "tz",
-    "ug",
-    "za",
-    "zm",
-    "zw",
-    "ly",
-    "ci",
-    "cd",
-    "mz",
-    "tn",
-    "re",
-    "km",
-  ],
-  Europe: [
-    "at",
-    "be",
-    "bg",
-    "hr",
-    "cy",
-    "cz",
-    "dk",
-    "ee",
-    "fi",
-    "fr",
-    "de",
-    "gr",
-    "hu",
-    "ie",
-    "it",
-    "lv",
-    "lt",
-    "lu",
-    "mt",
-    "nl",
-    "pl",
-    "pt",
-    "ro",
-    "sk",
-    "si",
-    "es",
-    "se",
-    "gb",
-    "is",
-    "no",
-    "ch",
-  ],
-  "East-Asia": ["cn", "jp", "kr", "mn", "tw", "hk"],
-  "South-Asia": ["af", "bd", "bt", "in", "mv", "np", "pk", "lk"],
-  America: [
-    "ag",
-    "ai",
-    "ar",
-    "bs",
-    "bb",
-    "bz",
-    "bo",
-    "br",
-    "ca",
-    "cl",
-    "co",
-    "cr",
-    "cu",
-    "do",
-    "ec",
-    "sv",
-    "gt",
-    "gy",
-    "hn",
-    "jm",
-    "mx",
-    "ni",
-    "pa",
-    "py",
-    "pe",
-    "sr",
-    "tt",
-    "us",
-    "uy",
-    "ve",
-    "gf",
-    "ht",
-  ],
-  "Middle-East": [
-    "ae",
-    "bh",
-    "iq",
-    "ir",
-    "il",
-    "jo",
-    "kw",
-    "lb",
-    "om",
-    "qa",
-    "sa",
-    "sy",
-    "tr",
-    "ye",
-  ],
-  "Central-Asia": ["kz", "kg", "tj", "tm", "uz"],
-  Australian: ["au", "nz"],
+let countriesByRegion = {
+  Africa: [],
+  Europe: [],
+  "East-Asia": [],
+  "South-Asia": [],
+  America: [],
+  "Middle-East": [],
+  "Central-Asia": [],
+  Australian: [],
 };
 
+const url = "../user-gestion/countries/get-codes/";
+// Función para llenar el diccionario
+async function fillCountriesByRegion(region) {
+  try {
+    const response = await axios.get(url, {
+      params: {
+        search: region,
+      },
+    });
+    countriesByRegion[region] = response.data;
+  } catch (error) {
+    console.error(`Error fetching data for ${region}:`, error);
+  }
+}
+
+// Lista de regiones
+const regions = Object.keys(countriesByRegion);
+
+// Llenar el objeto para todas las regiones
+async function fillAllRegions() {
+  const promises = regions.map((region) => fillCountriesByRegion(region));
+  await Promise.all(promises);
+  localStorage.setItem("countriesByRegion", JSON.stringify(countriesByRegion));
+}
 let countryColors = {};
 let countryColorsWhite = {};
-function getCountriesByRegion(region) {
+async function getCountriesByRegion(region) {
+  const storedData = localStorage.getItem("countriesByRegion");
+  // Llamar a la función para llenar todas las regiones
+  if (storedData) {
+    // Si hay datos, cargar desde localStorage
+    Object.assign(countriesByRegion, JSON.parse(storedData));
+  } else {
+    // Si no hay datos, llenar desde la API
+    await fillAllRegions();
+  }
+
   countryColors = {};
   // Reiniciar el mapa antes de aplicar nuevos colores
   resetMap();
@@ -197,17 +95,14 @@ function getCountriesByRegion(region) {
       countryColors[pais] = "#0000ff"; // Color azul para cada país
       countryColorsWhite[pais] = "#ffffff"; // Color azul para cada país
     });
-
     // Pintar los países en el mapa
     jQuery("#world-map").vectorMap("set", "colors", countryColors);
-
     // Mostrar el modal
     $("#modal-map").modal("show");
   } else {
     //console.log("Región no encontrada.");
   }
 }
-
 function resetMap() {
   jQuery("#world-map").vectorMap("set", "colors", countryColorsWhite);
 }
