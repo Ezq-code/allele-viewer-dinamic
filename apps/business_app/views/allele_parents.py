4d4b4f2a177f307b0e24a685049582004e9f8603
Drak <ealmeida.cu@gmail.com>
2025-04-08 17:08:11 -0400

    cached graph by pdb_file id

--- apps/business_app/views/allele_parents.py
+++ apps/business_app/views/allele_parents.py
@@ -1,11 +1,15 @@
 from django.http import JsonResponse
 from rest_framework import permissions, viewsets
 from rest_framework.generics import GenericAPIView
+from django.core.cache import cache
 
 
 from apps.business_app.serializers.allele_parents import AlleleParentsSerializer
 
-from apps.business_app.utils.xslx_to_pdb_graph import XslxToPdbGraph
+from apps.business_app.utils.xslx_to_pdb_graph import (
+    XslxToPdbGraph,
+    extract_parents_tree,
+)
 
 from apps.business_app.models.uploaded_files import UploadedFiles
 
@@ -26,13 +30,22 @@ class AlleleParentsViewSet(viewsets.ViewSet, GenericAPIView):
         serializer.is_valid(raise_exception=True)
         # Load parameters
         pdb = serializer.validated_data.get("pdb")
-        allele_node = serializer.validated_data.get("allele_node")
+
         # Filter the PDB File
         pdb_file = UploadedFiles.objects.get(id=pdb)
         # Create the Graph
-        processor_object = XslxToPdbGraph(pdb_file.original_file)
-        processor_object.proccess_initial_file_data(pdb)
-        # Ejecución del algoritmo que extrae los alleles parents (1574,131,5)
-        list_alleles = processor_object.proccess_allele_parents(allele_node)
+        graph_key = f"graph_for_{pdb_file.id}"
 
-        return JsonResponse(list_alleles, safe=False)
+        if not cache.get(graph_key):
+            processor_object = XslxToPdbGraph(
+                origin_file=pdb_file.original_file, uploaded_file_id=pdb_file.id
+            )
+            processor_object.proccess_initial_file_data(pdb)
+
+        # Ejecución del algoritmo que extrae los alleles parents (1574,131,5)
+        # list_alleles = processor_object.proccess_allele_parents(allele_node)
+        allele_node = serializer.validated_data.get("allele_node")
+        return JsonResponse(
+            list(set(extract_parents_tree(cache.get(graph_key), [], allele_node))),
+            safe=False,
+        )
