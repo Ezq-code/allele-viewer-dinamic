190ff4cbf63733424a5b08701373abc85211bf98
Enrique Almeida <ealmeida@example.com>
2025-09-20 14:28:44 -0400

    added EPs

---
+++ apps/business_app/serializers/gene_group_serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from apps.business_app.models.gene_group import GeneGroups
+from apps.business_app.serializers.gene_serializer import GeneSerializer
+
+
+class GeneGroupsSerializer(serializers.ModelSerializer):
+    genes=GeneSerializer(many=True)
+    class Meta:
+        model = GeneGroups
+        fields = [
+            "id",
+            "name",
+            "description",
+            "genes",
+        ]
