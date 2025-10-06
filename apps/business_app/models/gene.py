a4749de4a71744cc2517359f3fc8e966035e8b52
Enrique Almeida <ealmeida@example.com>
2025-10-06 12:40:59 -0400

    status and ordering

--- apps/business_app/models/gene.py
+++ apps/business_app/models/gene.py
@@ -3,17 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Gene(models.Model):
-    class Status(models.TextChoices):
-        COMPLETED = "C", _("Completed")
-        INCOMPLETED = "I", _("Not completed")
-
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
-    status = models.CharField(
-        max_length=1,
-        choices=Status.choices,
-        default=Status.INCOMPLETED,
-    )
+    status = models.SmallIntegerField(default=0)
 
     class Meta:
         verbose_name = _("Gene")
