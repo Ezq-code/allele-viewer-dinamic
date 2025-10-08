8133fb102b61a86da0dc1413f935d39cf6fd6a4e
ealmeida <ealmeida.cu@gmail.com>
2025-10-08 11:19:40 -0400

    gene list

--- apps/business_app/views/gene_view.py
+++ apps/business_app/views/gene_view.py
@@ -2,8 +2,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, viewsets
 from apps.business_app.models.gene import Gene
 from rest_framework.viewsets import GenericViewSet
-from apps.business_app.serializers.gene_serializer import GeneSerializer
+from apps.business_app.serializers.gene_serializer import (
+    GeneGetAllInfoSerializer,
+    GeneSerializer,
+)
 from apps.common.pagination import AllResultsSetPagination
+from rest_framework.decorators import action
 
 
 from apps.common.views import CommonOrderingFilter
@@ -37,3 +41,13 @@ class GeneViewSet(
     ]
     filterset_fields = ("status", "groups", "disorders")
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(
+        detail=False,
+        methods=["GET"],
+        url_path="get-all-info",
+        url_name="get-all-info",
+        serializer_class=GeneGetAllInfoSerializer,
+    )
+    def get_all_info(self, request):
+        return self.list(request)
