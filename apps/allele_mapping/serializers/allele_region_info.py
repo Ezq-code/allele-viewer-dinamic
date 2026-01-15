1213053c8872d68be01680859fd57188fd5aff61
Enrique Almeida Maldonado <ealmeida.cu@gmail.com>
2026-01-15 00:52:00 -0500

    improve speed to load allelemap (#284) (#285)
    Co-authored-by: angelrh83 <30694479+angelrh83@users.noreply.github.com>

--- apps/allele_mapping/serializers/allele_region_info.py
+++ apps/allele_mapping/serializers/allele_region_info.py
@@ -22,3 +22,27 @@ class AlleleRegionInfoDetailSerializer(serializers.ModelSerializer):
             'percent_of_individuals',
             'sample_size'
         ]
+
+
+class AlleleRegionInfoWithRegionSerializer(serializers.ModelSerializer):
+    allele_name = serializers.CharField(source='allele.name', read_only=True)
+    gene_name = serializers.CharField(source='allele.gene.name', read_only=True)
+    region_name = serializers.CharField(source='region.population', read_only=True)
+    region_location = serializers.CharField(source='region.location', read_only=True)
+    region_lat = serializers.FloatField(source='region.lat', read_only=True)
+    region_lon = serializers.FloatField(source='region.lon', read_only=True)
+
+    class Meta:
+        model = AlleleRegionInfo
+        fields = [
+            'id',
+            'allele_name',
+            'gene_name',
+            'allele_frequency',
+            'percent_of_individuals',
+            'sample_size',
+            'region_name',
+            'region_location',
+            'region_lat',
+            'region_lon'
+        ]
