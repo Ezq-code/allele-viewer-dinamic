fd812f8c8c5b4faa35ac9661474f9454c0296429
Enrique Almeida <ealmeida@example.com>
2026-02-17 01:41:45 -0500

    added filters

--- apps/allele_mapping/views/allele_region_info.py
+++ apps/allele_mapping/views/allele_region_info.py
@@ -31,9 +31,11 @@ class AlleleRegionInfoViewSet(viewsets.ReadOnlyModelViewSet):
         "region__population": ["icontains"],
         "allele__name": ["icontains"],
         "allele__gene__name": ["iexact"],
+        "sub_country__name": ["icontains"],
     }
     filterset_fields = {
         "region": ["exact"],
+        "sub_country": ["exact"],
         "allele": ["exact"],
         "allele__gene": ["exact"],
         "sample_size": ["gte", "lte"],
